05e5149c018a3dc2d5a2a75e2d26889e065114f5
conneR <tsygoogl@gmail.com>
2022-04-13 15:33:35 +0800

    11

--- app/src/main/java/com/example/newag/mvp/ui/input_period/other_period.java
+++ app/src/main/java/com/example/newag/mvp/ui/input_period/other_period.java
@@ -1,24 +1,36 @@
 package com.example.newag.mvp.ui.input_period;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.newag.R;
+import com.example.newag.mvp.adapter.AllTextMasterAdapter;
+import com.example.newag.mvp.model.bean.AllText;
+import com.example.newag.mvp.model.bean.AllTextMaster;
 import com.example.newag.mvp.ui.plus.period_plus;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class other_period extends AppCompatActivity {
@@ -29,6 +41,15 @@ public class other_period extends AppCompatActivity {
     Button tb1;
     Calendar calendar= Calendar.getInstance(Locale.CHINA);
     public Button btnDate;
+    private final List<AllText> allTextList11=new ArrayList<>();
+    private final List<AllText> allTextList22=new ArrayList<>();
+    private final List<AllText> allTextList1=new ArrayList<>();
+    private final List<AllText> allTextList2=new ArrayList<>();//定义一个新的arraylist,数据类型为自定义的AllText，基础数据
+    private final List<AllTextMaster> data_1=new ArrayList<>();//定义数据1,原始数据
+    private final List<AllTextMaster> data_2=new ArrayList<>();//定义数据2,模拟修改后的数据
+    private PopupWindow popupWindow;//定义一个新的popupWindow 主
+    private PopupWindow newPopWindow;//副
+    private AllTextMasterAdapter adapter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.period_other);
@@ -46,6 +67,31 @@ public class other_period extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showDatePickerDialog(other_period.this,  2, btnDate, calendar);;
+            }
+        });
+        initText();//为原始数据添加数据
+        RecyclerView recyclerView=findViewById(R.id.view_one);//找到布局中的recycleview控件
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);//设置布局管理器，cv工程
+        recyclerView.setLayoutManager(linearLayoutManager);//为recycleview添加布局管理器，cv
+         /*adapter=new AllTextAdapter(allTextList);//定义一个新的自定义适配器（AllTextAdapter），并且把数据传进去
+        recyclerView.setAdapter(adapter);//为recycleview传入定义好的适配器，并展示*/
+        adapter=new AllTextMasterAdapter(this,data_1);//定义一个新的大适配器（AllTextMasterAdapter），并且把数据传进去
+        recyclerView.setAdapter(adapter);//设置适配器
+        Button Button=findViewById(R.id.left);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopWindow();//展示popwindow的方法
+            }
+        });
+        //
+        SwipeRefreshLayout refreshLayout=findViewById(R.id.refresh);//找到下拉刷新
+        refreshLayout.setColorSchemeResources(R.color.blue,R.color.blue);//设置下拉刷新主题（最多支持三种颜色变换，这里两种）
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                adapter.setNewData(data_2);//模拟数据变换,以后这里就写从后端获取数据的逻辑
+                refreshLayout.setRefreshing(false);
             }
         });
         ce1.setOnClickListener(new View.OnClickListener() {
@@ -108,5 +154,74 @@ public class other_period extends AppCompatActivity {
                 , calendar.get(Calendar.YEAR)
                 , calendar.get(Calendar.MONTH)
                 , calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+    private void showPopWindow() {
+        //定义一个view，其中包含popwindow的布局文件
+        View view1= LayoutInflater.from(other_period.this).inflate(R.layout.test_popupwindow,null);
+        popupWindow =new PopupWindow(view1, RecyclerView.LayoutParams.MATCH_PARENT,
+                RecyclerView.LayoutParams.WRAP_CONTENT,true);//设置popwindow的属性（布局，x，y，true）
+        TextView make_text=(TextView)view1.findViewById(R.id.make_text);
+        TextView back_test=(TextView)view1.findViewById(R.id.back_test);
+        make_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.setCheckbox(true);
+                adapter.notifyDataSetChanged();
+                popupWindow.dismiss();//销毁popwindow
+                View rootView= LayoutInflater.from(other_period.this).inflate(R.layout.period_other,null);
+                newPopWindow.showAtLocation(rootView, Gravity.BOTTOM,0,0);
+            }
+        });
+        back_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+            }
+        });
+        //定义一个view，其中包含main4的布局文件
+        View rootView=LayoutInflater.from(other_period.this).inflate(R.layout.period_other,null);
+        popupWindow.showAtLocation(rootView, Gravity.BOTTOM,0,0);//展示自定义的popwindow，（放哪个布局里，放布局里的位置，x，y），cv工程
+        View view2=LayoutInflater.from(other_period.this).inflate(R.layout.popwindowdelete,null);
+        newPopWindow=new PopupWindow(view2,RecyclerView.LayoutParams.MATCH_PARENT,
+                RecyclerView.LayoutParams.WRAP_CONTENT,false);
+        Button button_delete=(Button) view2.findViewById(R.id.delete);
+        button_delete.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onClick(View view) {
+                adapter.setCheckbox(false);
+                adapter.notifyDataSetChanged();
+                newPopWindow.dismiss();
+            }
+        });
+    }
+    //添加数据相关方法
+    private void initText() {
+        AllText one=new AllText("one");
+        allTextList1.add(one);
+        AllText two=new AllText("two");
+        allTextList1.add(two);
+        AllText three=new AllText("three");
+        allTextList1.add(three);
+        AllTextMaster add1=new AllTextMaster("0",allTextList1);
+        data_1.add(add1);
+        allTextList2.add(one);
+        allTextList2.add(two);
+        AllTextMaster add2=new AllTextMaster("1",allTextList2);
+        data_1.add(add2);
+        //
+        AllText one1=new AllText("1.其他1\n");
+        allTextList11.add(one1);
+        AllText two2=new AllText("2.其他2\n");
+        allTextList11.add(two2);
+        AllText three3=new AllText("3.其他3\n");
+        allTextList11.add(three3);
+        AllTextMaster add11=new AllTextMaster("4月10日",allTextList11);
+        data_2.add(add11);
+        allTextList22.clear();
+        AllText one2=new AllText("4.其他4\n");
+        allTextList22.add(one2);
+        AllTextMaster add22=new AllTextMaster("4月9日",allTextList22);
+        data_2.add(add22);
     }
 }
