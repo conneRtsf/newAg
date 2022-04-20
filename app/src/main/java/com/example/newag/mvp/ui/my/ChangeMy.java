80c90f7242208e6bc135f930a21fb465adc28ba6
conneR <tsygoogl@gmail.com>
2022-04-20 13:53:25 +0800

    11

---
+++ app/src/main/java/com/example/newag/mvp/ui/my/ChangeMy.java
@@ -0,0 +1,20 @@
+package com.example.newag.mvp.ui.my;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newag.R;
+
+import butterknife.OnClick;
+
+public class ChangeMy extends AppCompatActivity {
+    @OnClick(R.id.back)
+    void Click1(){
+        ChangeMy.this.finish();
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changemy);
+    }
+}
